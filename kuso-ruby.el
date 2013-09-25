4a845dac0cb85e5bed1115ead926c29f3417847e
Sameer Rahmani <lxsameer@gnu.org>
2013-09-25 16:10:30 +0330

    initial commit

---
+++ kuso-ruby.el
@@ -0,0 +1,35 @@
+;;   Kuso IDE
+;;    Copyright (C) 2010-2013  Sameer Rahmani <lxsameer@gnu.org>
+;;
+;;    This program is free software: you can redistribute it and/or modify
+;;    it under the terms of the GNU General Public License as published by
+;;    the Free Software Foundation, either version 3 of the License, or
+;;    any later version.
+;;
+;;    This program is distributed in the hope that it will be useful,
+;;    but WITHOUT ANY WARRANTY; without even the implied warranty of
+;;    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;;    GNU General Public License for more details.
+;;
+;;    You should have received a copy of the GNU General Public License
+;;    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+;; Yaml-mode configurations
+(add-to-list 'auto-mode-alist '("\\.yml$" . yaml-mode))
+(add-hook 'yaml-mode-hook
+	  '(lambda ()
+	     (define-key yaml-mode-map "\C-m" 'newline-and-indent)))
+
+;; Inf Ruby configuration
+(autoload 'inf-ruby "inf-ruby" "Run an inferior Ruby process" t)
+(autoload 'inf-ruby-setup-keybindings "inf-ruby" "" t)
+
+(eval-after-load 'ruby-mode
+  '(add-hook 'ruby-mode-hook 'inf-ruby-setup-keybindings))
+
+;; Rinari configurations
+(setq rinari-tags-file-name "TAGS")
+
+;; RHTML configurations
+(add-hook 'rhtml-mode-hook
+     	  (lambda () (rinari-launch)))
