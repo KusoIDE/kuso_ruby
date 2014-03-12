6af1dcbd0a5f32ae3adbe524a6aa241ba9e5c2e7
Sameer Rahmani <lxsameer@gnu.org>
2014-03-12 18:45:15 +0330

    powerline highlight indent and project-explorer added

--- kuso-ruby.el
+++ kuso-ruby.el
@@ -16,10 +16,10 @@
 
 ;; Configuring rbenv
 (require 'rbenv)
-;(global-rbenv-mode)
+(global-rbenv-mode)
 
-(add-hook 'ruby-mode-hook
-     	  'global-rbenv-mode)
+;(add-hook 'ruby-mode-hook
+;     	  'global-rbenv-mode)
 
 ;; Ruby mode configurations
 (add-to-list 'auto-mode-alist '("\\.rake$" . ruby-mode))
@@ -35,11 +35,8 @@
 	     (define-key yaml-mode-map "\C-m" 'newline-and-indent)))
 
 ;; Inf Ruby configuration
-;(require 'inf-ruby)
 (autoload 'inf-ruby "inf-ruby" "Run an inferior Ruby process" t)
-;(autoload 'inf-ruby-minor-mode "inf-ruby" "Run an inferior Ruby process" t)
-;(add-hook 'ruby-mode-hook 'inf-ruby-minor-mode)
-(add-hook 'inf-ruby-mode-hook 'rbenv-patch)
+(add-hook 'ruby-mode-hook 'rbenv-patch)
 (add-hook 'after-init-hook 'inf-ruby-switch-setup)
 
 (defun rbenv-patch ()
@@ -47,11 +44,8 @@
   (setq irbpath (rbenv--expand-path "shims" "irb"))
   (setq irb (concat irbpath irbparams))
   (add-to-list 'inf-ruby-implementations (cons "ruby" irb))
-  (inf-ruby-minor-mode t)
-  (inf-ruby-console-auto)
+  ;(inf-ruby-minor-mode t)
   )
-
-
 
 (global-set-key (kbd "C-c r r") 'inf-ruby)
 ;; The default indentation system attempts to align the arguments of a function
@@ -61,7 +55,6 @@
 ;; the following will make emacs indent code inside parenthesis similar to
 ;;  elsewhere.
 (setq ruby-deep-indent-paren nil)
-
 (add-hook 'ruby-mode-hook 'projectile-on)
 
 ;; Robe mode
@@ -77,11 +70,26 @@
 (add-hook 'ruby-mode-hook 'flymake-ruby-load)
 
 
-;; Rinari configurations
-(setq rinari-tags-file-name "TAGS")
-
-;; RHTML configurations
-(add-hook 'rhtml-mode-hook
-     	  (lambda () (rinari-launch)))
-
 (add-hook 'ruby-mode-hook (lambda () (autopair-global-mode -1)))
+
+;; Power Line
+(require 'powerline)
+
+;; highlight indents
+(require 'highlight-indentation)
+;; TODO: move these to a theme
+(set-face-background 'highlight-indentation-face "#383a30")
+(set-face-background 'highlight-indentation-current-column-face "#494d38")
+(add-hook 'ruby-mode-hook 'highlight-indentation-mode)
+
+(setq powerline-arrow-shape 'arrow)   ;; the default
+(setq powerline-arrow-shape 'curve)   ;; give your mode-line curves
+(setq powerline-arrow-shape 'arrow14) ;; best for small fonts
+(custom-set-faces
+ '(mode-line ((t (:foreground "#030303" :background "#bdbdbd" :box nil))))
+ '(mode-line-inactive ((t (:foreground "#f9f9f9" :background "#666666" :box nil)))))
+(setq powerline-color1 "grey22")
+(setq powerline-color2 "grey40")
+
+(require 'es-lib)
+(require 'es-windows)
